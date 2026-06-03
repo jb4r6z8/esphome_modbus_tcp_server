90cf28ebf01695787dc03000243f3eaccdb8e5c0
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-06-03 20:34:34 +0200

    VSCODE

--- components/modbus_tcp_server/__init__.py
+++ components/modbus_tcp_server/__init__.py
@@ -5,7 +5,7 @@ from esphome.components import modbus
 
 MULTI_CONF = True
 CODEOWNERS = []
-AUTO_LOAD = ["MODBUS"]
+AUTO_LOAD = ["modbus"]
 
 
 modbus_tcp_server_ns = cg.esphome_ns.namespace("modbus_tcp_server")
